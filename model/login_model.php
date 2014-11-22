5d9f424951696e171d0226e96f8bc3ff5f8a7614
EmperorD6 <darrylearandia@gmail.com>
2014-11-23 07:40:27 +0800

    Model Files Commited

---
+++ model/login_model.php
@@ -0,0 +1,22 @@
+<?php
+
+function get_user($username){
+include "config/conn.php";
+
+ $result = mysqli_query ($cxn,"SELECT * FROM create_user  WHERE username_='".$username."'") ;
+  
+ return $result;
+
+}
+
+/*
+function clean_input($input){
+$input=trim($input);
+$input=stripslashes($input);
+$input=htmlspecialchars($input);
+return $input;
+}
+*/
+
+
+?>	
