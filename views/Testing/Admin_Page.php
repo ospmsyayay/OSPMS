db3d0d794db5c5e8e4ac6ee9f4efc04fa1b5f673
rizdcny <rizzadacanay621@yahoo.com>
2014-12-05 09:29:12 +0800

    views\Testing\Admin_Page
    Admin Page Design

--- views/Testing/Admin_Page.php
+++ views/Testing/Admin_Page.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta charset="utf-8">
-		<title>Bootstrap 3 Admin</title>
+		<title>Online Student Performance Monitoring System</title>
 		<meta name="generator" content="Bootply" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link href="views/TESTING/css/bootstrap.min.css" rel="stylesheet">
@@ -19,18 +19,11 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Dashboard</a>
+      <a class="navbar-brand" href="#">Admin</a>
     </div>
     <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav navbar-right">
-        
-        <li class="dropdown">
-          <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-user"></i> Admin <span class="caret"></span></a>
-          <ul id="g-account-menu" class="dropdown-menu" role="menu">
-            <li><a href="#">My Profile</a></li>
-          </ul>
-        </li>
-        <li><a href="#"><i class="glyphicon glyphicon-lock"></i> Logout</a></li>
+		
       </ul>
     </div>
   </div><!-- /container -->
@@ -42,7 +35,7 @@
 <div class="row">
 	<div class="col-sm-3">
       <!-- Left column -->
-      <a href="#"><strong><i class="glyphicon glyphicon-wrench"></i> Tools</strong></a>  
+      <a href="#"><strong><i class="glyphicon glyphicon-wrench"></i> Navigation</strong></a>  
       
       <hr>
       
@@ -55,9 +48,6 @@
                 <li><a href="#"><i class="glyphicon glyphicon-envelope"></i> Messages <span class="badge badge-info">4</span></a></li>
                 <li><a href="#"><i class="glyphicon glyphicon-cog"></i> Options</a></li>
                 <li><a href="#"><i class="glyphicon glyphicon-comment"></i> Shoutbox</a></li>
-                <li><a href="#"><i class="glyphicon glyphicon-user"></i> Staff List</a></li>
-                <li><a href="#"><i class="glyphicon glyphicon-flag"></i> Transactions</a></li>
-                <li><a href="#"><i class="glyphicon glyphicon-exclamation-sign"></i> Rules</a></li>
                 <li><a href="#"><i class="glyphicon glyphicon-off"></i> Logout</a></li>
             </ul>
         </li>
@@ -66,89 +56,49 @@
           </a>
         
             <ul class="list-unstyled collapse" id="menu2">
-                <li><a href="#">Information &amp; Stats</a>
-                </li>
-                <li><a href="#">Views</a>
-                </li>
-                <li><a href="#">Requests</a>
-                </li>
-                <li><a href="#">Timetable</a>
-                </li>
-                <li><a href="#">Alerts</a>
+                <li><a href="#">Teacher</a></li>
+                <li><a href="#">Student</a></li>
+                <li><a href="#">Parents</a></li>
                 </li>
             </ul>
         </li>
         <li class="nav-header">
         <a href="#" data-toggle="collapse" data-target="#menu3">
-          <h5>Social Media <i class="glyphicon glyphicon-chevron-right"></i></h5>
+          <h5>Progress <i class="glyphicon glyphicon-chevron-right"></i></h5>
           </a>
         
             <ul class="list-unstyled collapse" id="menu3">
-                <li><a href="#"><i class="glyphicon glyphicon-circle"></i> Facebook</a></li>
-                <li><a href="#"><i class="glyphicon glyphicon-circle"></i> Twitter</a></li>
+                <li><a href="#"><i class="glyphicon glyphicon-circle"></i> Student Progress</a></li>
             </ul>
         </li>
       </ul>
            
       <hr>
       
-      <a href="#"><strong><i class="glyphicon glyphicon-link"></i> Resources</strong></a>  
       
-      <hr>
-      
-      <ul class="nav nav-pills nav-stacked">
-        <li class="nav-header"></li>
-        <li><a href="#"><i class="glyphicon glyphicon-list"></i> Layouts &amp; Templates</a></li>
-        <li><a href="#"><i class="glyphicon glyphicon-briefcase"></i> Toolbox</a></li>
-        <li><a href="#"><i class="glyphicon glyphicon-link"></i> Widgets</a></li>
-        <li><a href="#"><i class="glyphicon glyphicon-list-alt"></i> Reports</a></li>
-        <li><a href="#"><i class="glyphicon glyphicon-book"></i> Pages</a></li>
-        <li><a href="#"><i class="glyphicon glyphicon-star"></i> Social Media</a></li>
-      </ul>
-      
-      <hr>
-      <ul class="nav nav-stacked">
-        <li class="active"><a href="http://bootply.com" title="The Bootstrap Playground" target="ext">Playground</a></li>
-        <li><a href="/tagged/bootstrap-3">Bootstrap 3</a></li>
-        <li><a href="/61518" title="Bootstrap 3 Panel">Panels</a></li>
-        <li><a href="/61521" title="Bootstrap 3 Icons">Glyphicons</a></li>
-        <li><a href="/62603">Layout</a></li>
-      </ul>
-
-      <hr>
-      
-      <a href="#"><strong>Want More Templates?</strong></a>  
-      
-      <hr>
-      
-      <ul class="nav nav-stacked">
-        <li class="active"><a rel="nofollow" href="http://goo.gl/pQoXEh" target="ext">Premium Themes</a></li>
-        <li><a rel="nofollow" href="http://gridgum.com/themes/category/bootstrap-themes/?affiliates=45">GridGum</a></li>
-        <li><a rel="nofollow" href="http://bootstrapzero.com">BootstrapZero</a></li>
-      </ul>
       
       
   	</div><!-- /col-3 -->
     <div class="col-sm-9">
       	
       <!-- column 2 -->	
-      <ul class="list-inline pull-right">
+     <!--<ul class="list-inline pull-right">
          <li><a href="#"><i class="glyphicon glyphicon-cog"></i></a></li>
-         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-comment"></i><span class="count">3</span></a><ul class="dropdown-menu" role="menu"><li><a href="#">1. Is there a way..</a></li><li><a href="#">2. Hello, admin. I would..</a></li><li><a href="#"><strong>All messages</strong></a></li></ul></li>
+         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-comment"></i><span class="count">3</span></a><ul class="dropdown-menu" role="menu">
+		 <li><a href="#">1. Is there a way..</a></li><li><a href="#">2. Hello, admin. I would..</a></li><li><a href="#"><strong>All messages</strong></a></li></ul></li>
          <li><a href="#"><i class="glyphicon glyphicon-user"></i></a></li>
          <li><a title="Add Widget" data-toggle="modal" href="#addWidgetModal"><span class="glyphicon glyphicon-plus-sign"></span> Add Widget</a></li>
-      </ul>
-      <a href="#"><strong><i class="glyphicon glyphicon-dashboard"></i> My Dashboard</strong></a>  
+      </ul>-->
+	  
+      <a href="#"><strong><i class="glyphicon glyphicon-dashboard"></i> Admin Dashboard</strong></a>  
       
       	<hr>
       
 		<div class="row">
-           
-            
-          
+                    
             <!-- center left-->	
          	<div class="col-md-6">
-			  <div class="well">Inbox Messages <span class="badge pull-right">3</span></div>
+			  <div class="well">Inbox Messages <span class="badge pull-right">4</span></div>
               
               <hr>
               
@@ -209,66 +159,33 @@
 
 			  <!--tabs-->
               <div class="container">
-                <div class="col-md-4">
-                <ul class="nav nav-tabs" id="myTab">
-                  <li class="active"><a href="#profile" data-toggle="tab">Profile</a></li>
-                  <li><a href="#messages" data-toggle="tab">Messages</a></li>
-                  <li><a href="#settings" data-toggle="tab">Settings</a></li>
-                </ul>
-                
-                <div class="tab-content">
-                  <div class="tab-pane active" id="profile">
-                    <h4><i class="glyphicon glyphicon-user"></i></h4>
-                    Lorem profile dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-                    <p>Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-                      dolor, in sagittis nisi.</p>
-                  </div>
-                  <div class="tab-pane" id="messages">
-                    <h4><i class="glyphicon glyphicon-comment"></i></h4>
-                    Message ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-                    <p>Quisque mauris augu.</p>
-                  </div>
-                  <div class="tab-pane" id="settings">
-                    <h4><i class="glyphicon glyphicon-cog"></i></h4>
-                    Lorem settings dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-                    <p>Quisque mauris augue, molestie.</p>
-                  </div>
-                </div>
+					<div class="col-md-4">
+					<ul class="nav nav-tabs" id="myTab">
+					  <li class="active"><a href="#profile" data-toggle="tab">Profile</a></li>
+					  <li><a href="#messages" data-toggle="tab">Messages</a></li>
+					</ul>
+					
+						<div class="tab-content">
+						  <div class="tab-pane active" id="profile">
+							 <br/>
+							Profile of the Admin
+							<p>Name</p>
+						  </div>
+						  <div class="tab-pane" id="messages">
+							<br/>
+						   Message
+						  </div>
+						</div>
+					</div>
               	</div>
               </div>  
                
               <!--/tabs-->
               
-              <hr>
-              
-              <div class="panel panel-default">
-                  <div class="panel-heading"><h4>New Requests</h4></div>
-                  <div class="panel-body">
-                    <div class="list-group">
-                    <a href="#" class="list-group-item active">Hosting virtual mailbox serv..</a>
-                    <a href="#" class="list-group-item">Dedicated server doesn't..</a>
-                    <a href="#" class="list-group-item">RHEL 6 install on new..</a>
-                    </div>
-                  </div>
-              </div>
-   
+             
+          
           	</div><!--/col-->
         	<div class="col-md-6">
-				<div class="panel panel-default">
-                  <div class="panel-heading"><h4>Notices</h4></div>
-                  <div class="panel-body">
-                    
-                  <div class="alert alert-info">
-                    <button type="button" class="close" data-dismiss="alert">×</button>
-                    This is a dismissable alert.. just sayin'.
-                  </div>
-
-                  This is a dashboard-style layout that uses Bootstrap 3. You can use this template as a starting point to create something more unique.
-                  <br><br>
-                  Visit the Bootstrap Playground at <a href="http://bootply.com">Bootply</a> to tweak this layout or discover more useful code snippets.
-                  </div>
-              	</div>
-              
                 <table class="table table-striped">
                       <thead>
                         <tr><th>Visits</th><th>ROI</th><th>Source</th></tr>
@@ -328,14 +245,6 @@
                 
                   </div><!--/panel content-->
                 </div><!--/panel-->
-              
-              	<div class="panel panel-default">
-                  <div class="panel-heading"><div class="panel-title"><h4>Engagement</h4></div></div>
-                  <div class="panel-body">	
-                   	<div class="col-xs-4 text-center"><img src="http://placehold.it/80/BBBBBB/FFF" class="img-circle img-responsive"></div>
-                    <div class="col-xs-4 text-center"><img src="http://placehold.it/80/EFEFEF/555" class="img-circle img-responsive"></div>
-                    <div class="col-xs-4 text-center"><img src="http://placehold.it/80/EEEEEE/222" class="img-circle img-responsive"></div>
-                  </div>
                </div><!--/panel-->
               
 			</div><!--/col-span-6-->
@@ -344,28 +253,17 @@
       
       <hr>
       
-      <a href="#"><strong><i class="glyphicon glyphicon-comment"></i> Discussions</strong></a>  
+	  
+    
       
       <hr>
-      
-      <div class="row">
-        <div class="col-md-12">
-          <ul class="list-group">
-            <li class="list-group-item"><a href="#"><i class="glyphicon glyphicon-flash"></i> <small>(3 mins ago)</small> The 3rd page reports don't contain any links. Does anyone know why..</a></li>
-            <li class="list-group-item"><a href="#"><i class="glyphicon glyphicon-flash"></i> <small>(1 hour ago)</small> Hi all, I've just post a report that show the relationship betwe..</a></li>
-            <li class="list-group-item"><a href="#"><i class="glyphicon glyphicon-heart"></i> <small>(2 hrs ago)</small> Paul. That document you posted yesterday doesn't seem to contain the over..</a></li>
-            <li class="list-group-item"><a href="#"><i class="glyphicon glyphicon-heart-empty"></i> <small>(4 hrs ago)</small> The map service on c243 is down today. I will be fixing the..</a></li>
-            <li class="list-group-item"><a href="#"><i class="glyphicon glyphicon-heart"></i> <small>(yesterday)</small> I posted a new document that shows how to install the services layer..</a></li>
-            <li class="list-group-item"><a href="#"><i class="glyphicon glyphicon-flash"></i> <small>(yesterday)</small> ..</a></li>
-          </ul>
-        </div>
-      </div>
   	</div><!--/col-span-9-->
-</div>
+
 </div>
 <!-- /Main -->
 
-<footer class="text-center">This Bootstrap 3 dashboard layout is compliments of <a href="http://www.bootply.com/85850"><strong>Bootply.com</strong></a></footer>
+<footer class="text-center">Created by Arandia and Dacanay<a href="http://www.bootply.com/85850"></a></footer>
+
 
 <div class="modal" id="addWidgetModal">
   <div class="modal-dialog">
@@ -384,9 +282,6 @@
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dalog -->
 </div><!-- /.modal -->
-
-
-
   
 	<!-- script references -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
